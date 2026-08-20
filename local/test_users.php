d8de9028fcc0e16a5c59f43fe1a90b8760735235
louisrkz <louis.maximillian@student.ukdc.ac.id>
2026-08-20 10:35:17 +0700

    chore: ponytail audit cleanup (remove test scripts and unused files)

--- local/test_users.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=192.168.2.12;port=3306;dbname=keuangan', 'anugrah', 'anugrah'); 
-$stmt = $db->query('SELECT * FROM prm_users'); 
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
